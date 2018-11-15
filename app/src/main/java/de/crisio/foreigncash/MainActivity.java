5d3e86ff134d11e7bf9846b83694cdf52e903f16
CrisioDev <crisioletsplay@gmail.com>
2018-11-15 02:23:24 +0100

    designed first login Tab

--- app/src/main/java/de/crisio/foreigncash/MainActivity.java
+++ app/src/main/java/de/crisio/foreigncash/MainActivity.java
@@ -1,7 +1,10 @@
 package de.crisio.foreigncash;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.design.widget.TabLayout;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,19 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Create the adapter that will return a fragment for each of the three
+        // primary sections of the activity.
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        SectionsPagesAdapter mSectionsPagerAdapter = new SectionsPagesAdapter(getSupportFragmentManager());
+
+        // Set up the ViewPager with the sections adapter.
+        ViewPager mViewPager = (ViewPager) findViewById(R.id.container);
+        mViewPager.setAdapter(mSectionsPagerAdapter);
+
+        TabLayout tabLayout = (TabLayout) findViewById(R.id.tabLayout);
+        tabLayout.setupWithViewPager(mViewPager);
+
     }
 }
---
+++ app/src/main/java/de/crisio/foreigncash/SectionsPagesAdapter.java
@@ -0,0 +1,49 @@
+package de.crisio.foreigncash;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+
+public class SectionsPagesAdapter extends FragmentPagerAdapter {
+
+
+    public SectionsPagesAdapter(FragmentManager fm) {
+        super(fm);
+    }
+
+    @Override
+    public Fragment getItem(int position) {
+        // getItem is called to instantiate the fragment for the given page.
+        // Return a PlaceholderFragment (defined as a static inner class below).
+        switch (position) {
+            case 0:
+                return new Tab1();
+            case 1:
+                return new Tab2();
+            case 2:
+                return new Tab3();
+            default:
+                return null;
+        }
+    }
+
+    @Override
+    public int getCount() {
+        // Show 3 total pages.
+        return 3;
+    }
+
+    @Override
+    public CharSequence getPageTitle(int position) {
+        switch (position) {
+            case 0:
+                return "Tab 1";
+            case 1:
+                return "Tab 2";
+            case 2:
+                return "Tab 3";
+        }
+        return null;
+
+    }
+
+}
